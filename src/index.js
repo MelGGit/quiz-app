d31a27d655fe556a0f33ae0d267497dbc90e5f61
Mel Grosser <grosser.mel@live.de>
2021-04-27 15:35:43 +0200

    Basic JS and added index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// const pageHome = document.querySelector('page-home')
+// const pageBookmark = document.querySelector('page-bookmark')
+// const pageCreate = document.querySelector('page-create')
+// const pageProfile = document.querySelector('page-profile')
