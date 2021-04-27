4368b8398d5542bb72d8bedc328bd32960bfe70c
Mel Grosser <grosser.mel@live.de>
2021-04-27 16:19:05 +0200

    Added javascript implementation for all pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,67 @@
-// const pageHome = document.querySelector('page-home')
-// const pageBookmark = document.querySelector('page-bookmark')
-// const pageCreate = document.querySelector('page-create')
-// const pageProfile = document.querySelector('page-profile')
+const pageHome = document.querySelector('.page-home')
+const pageBookmark = document.querySelector('.page-bookmark')
+const pageCreate = document.querySelector('.page-create')
+const pageProfile = document.querySelector('.page-profile')
+
+const buttonHome = document.querySelector('.nav-bar__home')
+const buttonBookmark = document.querySelector('.nav-bar__bookmark')
+const buttonCreate = document.querySelector('.nav-bar__create')
+const buttonProfile = document.querySelector('.nav-bar__profile')
+
+const header = document.querySelector('.app-header__heading')
+
+buttonHome.addEventListener('click', () => {
+  pageHome.classList.remove('hidden')
+  pageBookmark.classList.add('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.add('hidden')
+
+  buttonHome.classList.add('active')
+  buttonBookmark.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.remove('active')
+
+  header.innerHTML = 'Quiz App'
+})
+
+buttonBookmark.addEventListener('click', () => {
+  pageHome.classList.add('hidden')
+  pageBookmark.classList.remove('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.add('hidden')
+
+  buttonHome.classList.remove('active')
+  buttonBookmark.classList.add('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.remove('active')
+
+  header.innerHTML = 'Bookmarks'
+})
+
+buttonCreate.addEventListener('click', () => {
+  pageHome.classList.add('hidden')
+  pageBookmark.classList.add('hidden')
+  pageCreate.classList.remove('hidden')
+  pageProfile.classList.add('hidden')
+
+  buttonHome.classList.remove('active')
+  buttonBookmark.classList.remove('active')
+  buttonCreate.classList.add('active')
+  buttonProfile.classList.remove('active')
+
+  header.innerHTML = 'Create'
+})
+
+buttonProfile.addEventListener('click', () => {
+  pageHome.classList.add('hidden')
+  pageBookmark.classList.add('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.remove('hidden')
+
+  buttonHome.classList.remove('active')
+  buttonBookmark.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonProfile.classList.add('active')
+
+  header.innerHTML = 'Profile'
+})
