63a7116963f5a62ce19513a2e8c80301f0becd2e
Mel Grosser <grosser.mel@live.de>
2021-05-03 15:43:56 +0200

    Header reorganized

--- src/setupNavigation.js
+++ src/setupNavigation.js
@@ -1,22 +1,36 @@
 import getElements from './utils/getElements'
+import getElement from './utils/getElement'
 
 export default function setupNavigation() {
   const pages = getElements('[data-page]')
   const buttons = getElements('[data-nav]')
+  const header = getElement('[data-header]')
 
   buttons.forEach(button => {
     const buttonName = button.dataset.nav
-    button.addEventListener('click', () => {
-      pages.forEach(page => {
-        const pageName = page.dataset.page
-        page.classList.toggle('hidden', pageName !== buttonName)
-      })
+    const headerText = button.dataset.headerText
 
-      console.log(buttonName)
-      console.log(button)
-      buttons.forEach(button => {
-        button.classList.toggle('active', buttonName === button.dataset.nav)
-      })
+    button.addEventListener('click', () => {
+      changePage(buttonName)
+      toggleButton(buttonName)
+      updateHeader(headerText)
     })
   })
+
+  function changePage(buttonName) {
+    pages.forEach(page => {
+      const pageName = page.dataset.page
+      page.classList.toggle('hidden', pageName !== buttonName)
+    })
+  }
+
+  function toggleButton(buttonName) {
+    buttons.forEach(button => {
+      button.classList.toggle('active', buttonName === button.dataset.nav)
+    })
+  }
+
+  function updateHeader(headerText) {
+    header.innerHTML = headerText
+  }
 }
