46dbe49172d71dfdf42afaafe42ff8a79616e7ed
Mel Grosser <grosser.mel@live.de>
2021-05-03 14:08:45 +0200

    nav changes

--- src/setupNavigation.js
+++ src/setupNavigation.js
@@ -1,67 +1,22 @@
-import getElement from './utils/getElement'
+import getElements from './utils/getElements'
 
 export default function setupNavigation() {
-  const pageHome = getElement('.page-home')
-  const pageBookmark = getElement('.page-bookmark')
-  const pageCreate = getElement('.page-create')
-  const pageProfile = getElement('.page-profile')
+  const pages = getElements('[data-page]')
+  const buttons = getElements('[data-nav]')
 
-  const buttonHome = getElement('.nav-bar__home')
-  const buttonBookmark = getElement('.nav-bar__bookmark')
-  const buttonCreate = getElement('.nav-bar__create')
-  const buttonProfile = getElement('.nav-bar__profile')
+  buttons.forEach(button => {
+    const buttonName = button.dataset.nav
+    button.addEventListener('click', () => {
+      pages.forEach(page => {
+        const pageName = page.dataset.page
+        page.classList.toggle('hidden', pageName !== buttonName)
+      })
 
-  const header = getElement('.app-header__heading')
-
-  buttonHome.addEventListener('click', () => {
-    setActivePage(pageHome)
-    setActiveButton(buttonHome)
-    setHeaderText('Quiz App')
+      console.log(buttonName)
+      console.log(button)
+      buttons.forEach(button => {
+        button.classList.toggle('active', buttonName === button.dataset.nav)
+      })
+    })
   })
-
-  buttonBookmark.addEventListener('click', () => {
-    setActivePage(pageBookmark)
-    setActiveButton(buttonBookmark)
-    setHeaderText('Bookmarks')
-  })
-
-  buttonCreate.addEventListener('click', () => {
-    setActivePage(pageCreate)
-    setActiveButton(buttonCreate)
-    setHeaderText('Create')
-  })
-
-  buttonProfile.addEventListener('click', () => {
-    setActivePage(pageProfile)
-    setActiveButton(buttonProfile)
-    setHeaderText('Profile')
-  })
-
-  function setActivePage(page) {
-    hideAllPages()
-    page.classList.remove('hidden')
-  }
-
-  function setActiveButton(button) {
-    inactivateAllButtons()
-    button.classList.add('active')
-  }
-
-  function hideAllPages() {
-    pageHome.classList.add('hidden')
-    pageBookmark.classList.add('hidden')
-    pageCreate.classList.add('hidden')
-    pageProfile.classList.add('hidden')
-  }
-
-  function inactivateAllButtons() {
-    buttonHome.classList.remove('active')
-    buttonBookmark.classList.remove('active')
-    buttonCreate.classList.remove('active')
-    buttonProfile.classList.remove('active')
-  }
-
-  function setHeaderText(text) {
-    header.innerText = text
-  }
 }
