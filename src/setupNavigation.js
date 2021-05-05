e1161862abfa9954755d2e6305091b960f7fbf81
Mel <60812382+MelGGit@users.noreply.github.com>
2021-05-05 15:48:12 +0200

    Nav (#22)
    * nav changes
    
    * Header reorganized

--- src/setupNavigation.js
+++ src/setupNavigation.js
@@ -1,67 +1,36 @@
+import getElements from './utils/getElements'
 import getElement from './utils/getElement'
 
 export default function setupNavigation() {
-  const pageHome = getElement('.page-home')
-  const pageBookmark = getElement('.page-bookmark')
-  const pageCreate = getElement('.page-create')
-  const pageProfile = getElement('.page-profile')
+  const pages = getElements('[data-page]')
+  const buttons = getElements('[data-nav]')
+  const header = getElement('[data-header]')
 
-  const buttonHome = getElement('.nav-bar__home')
-  const buttonBookmark = getElement('.nav-bar__bookmark')
-  const buttonCreate = getElement('.nav-bar__create')
-  const buttonProfile = getElement('.nav-bar__profile')
+  buttons.forEach(button => {
+    const buttonName = button.dataset.nav
+    const headerText = button.dataset.headerText
 
-  const header = getElement('.app-header__heading')
-
-  buttonHome.addEventListener('click', () => {
-    setActivePage(pageHome)
-    setActiveButton(buttonHome)
-    setHeaderText('Quiz App')
+    button.addEventListener('click', () => {
+      changePage(buttonName)
+      toggleButton(buttonName)
+      updateHeader(headerText)
+    })
   })
 
-  buttonBookmark.addEventListener('click', () => {
-    setActivePage(pageBookmark)
-    setActiveButton(buttonBookmark)
-    setHeaderText('Bookmarks')
-  })
-
-  buttonCreate.addEventListener('click', () => {
-    setActivePage(pageCreate)
-    setActiveButton(buttonCreate)
-    setHeaderText('Create')
-  })
-
-  buttonProfile.addEventListener('click', () => {
-    setActivePage(pageProfile)
-    setActiveButton(buttonProfile)
-    setHeaderText('Profile')
-  })
-
-  function setActivePage(page) {
-    hideAllPages()
-    page.classList.remove('hidden')
+  function changePage(buttonName) {
+    pages.forEach(page => {
+      const pageName = page.dataset.page
+      page.classList.toggle('hidden', pageName !== buttonName)
+    })
   }
 
-  function setActiveButton(button) {
-    inactivateAllButtons()
-    button.classList.add('active')
+  function toggleButton(buttonName) {
+    buttons.forEach(button => {
+      button.classList.toggle('active', buttonName === button.dataset.nav)
+    })
   }
 
-  function hideAllPages() {
-    pageHome.classList.add('hidden')
-    pageBookmark.classList.add('hidden')
-    pageCreate.classList.add('hidden')
-    pageProfile.classList.add('hidden')
-  }
-
-  function inactivateAllButtons() {
-    buttonHome.classList.remove('active')
-    buttonBookmark.classList.remove('active')
-    buttonCreate.classList.remove('active')
-    buttonProfile.classList.remove('active')
-  }
-
-  function setHeaderText(text) {
-    header.innerText = text
+  function updateHeader(headerText) {
+    header.innerHTML = headerText
   }
 }
